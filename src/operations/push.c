4e7446f0201e2228675dd7d7374c36c6d09168b7
itslucyax <luciameslava@gmail.com>
2026-05-28 21:01:24 +0200

    en busca del kali

---
+++ src/operations/push.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amendibi <amendibi@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 17:43:19 by amendibi          #+#    #+#             */
+/*   Updated: 2026/05/28 20:59:47 by amendibi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	pb(t_stack *backpack)
+{
+	int	i;
+
+	if(backpack->len_a < 1)
+		return ;
+	i = backpack->len_b ;
+	while (i > 0)
+	{
+ 		backpack->stack_b[i] = backpack->stack_b[i - 1];
+		i--;
+ 	}
+	backpack->stack_b[0] = backpack->stack_a[0];
+	backpack->len_b++;
+	i = 0;
+	while (i < backpack->len_a - 1)
+	{
+		backpack->stack_a[i] = backpack->stack_a[i + 1];
+		i++;
+	}
+	backpack->len_a--;
+	write(1, "pb\n", 3);
+}
+
+void	pa(t_stack *backpack)
+{
+	int	i;
+
+	if(backpack->len_b < 1)
+		return ;
+	i = backpack->len_a ;
+	while (i > 0)
+	{
+ 		backpack->stack_a[i] = backpack->stack_a[i - 1];
+		i--;
+ 	}
+	backpack->stack_a[0] = backpack->stack_b[0];
+	backpack->len_a++;
+	i = 0;
+	while (i < backpack->len_b - 1)
+	{
+		backpack->stack_b[i] = backpack->stack_b[i + 1];
+		i++;
+	}
+	backpack->len_b--;
+	write(1, "pa\n", 3);
+}
