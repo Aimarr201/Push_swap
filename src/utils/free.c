719b610f61b1ba7f7d9856227909a926117be58d
Aimar <mendibil.ayo@gmail.com>
2026-05-29 20:16:58 +0200

    ups

--- src/utils/free.c
+++ src/utils/free.c
@@ -6,20 +6,21 @@
 /*   By: amendibi <amendibi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 18:29:03 by luciamar          #+#    #+#             */
-/*   Updated: 2026/05/28 18:32:44 by amendibi         ###   ########.fr       */
+/*   Updated: 2026/05/29 18:52:17 by amendibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	free_arrays(t_stacks *backpack)
+void	ft_free_arrays(t_stack *backpack)
 {
 	if (backpack->stack_a)
 	{
 		free(backpack->stack_a);
 		backpack->stack_a = NULL;
+
 	}
-	if (backpack->array_b)
+	if (backpack->stack_b)
 	{
 		free(backpack->stack_b);
 		backpack->stack_b = NULL;
