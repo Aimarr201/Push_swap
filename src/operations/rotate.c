a74d4a5650ecece4c373763e35ac1ea8810a68c6
itslucyax <luciameslava@gmail.com>
2026-05-29 20:15:59 +0200

    movimiendos rotate

---
+++ src/operations/rotate.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amendibi <amendibi@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/29 17:21:39 by luciamar          #+#    #+#             */
+/*   Updated: 2026/05/29 19:44:53 by amendibi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ra(t_stack *backpack)
+{
+	int	tmp;
+	int	i;
+
+	if (backpack->len_a < 2)
+		return;
+	tmp = backpack->stack_a[0];
+	i = 0;
+	while (i < backpack->len_a - 1)
+	{
+		backpack->stack_a[i] = backpack->stack_a[i + 1];
+		i++;
+	}
+	backpack->stack_a[backpack->len_a - 1] = tmp;
+	ft_putstr("ra\n");
+}
+
+void	rb(t_stack *backpack)
+{
+	int	tmp;
+	int	i;
+
+	if (backpack->len_b < 2)
+		return;
+	tmp = backpack->stack_b[0];
+	i = 0;
+	while (i < backpack->len_b - 1)
+	{
+		backpack->stack_b[i] = backpack->stack_b[i + 1];
+		i++;
+	}
+	backpack->stack_b[backpack->len_b - 1] = tmp;
+	ft_putstr("rb\n");
+}
+
+void	rr(t_stack *backpack)
+{
+	int	tmp;
+	int	i;
+
+	if (backpack->len_a >= 2)
+	{
+		tmp = backpack->stack_a[0];
+		i = -1;
+		while (++i < backpack->len_a - 1)
+			backpack->stack_a[i] = backpack->stack_a[i + 1];
+			i++;
+		backpack->stack_a[backpack->len_a - 1] = tmp;
+	}
+	if (backpack->len_b >= 2)
+	{
+		tmp = backpack->stack_b[0];
+		i = -1;
+		while (++i < backpack->len_b - 1)
+			backpack->stack_b[i] = backpack->stack_b[i + 1];
+			i++;
+		backpack->stack_b[backpack->len_b - 1] = tmp;
+	}
+	ft_putstr("rr\n");
+}
