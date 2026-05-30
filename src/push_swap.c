eda89cb8431165c16e226224638dc0b1e9d5e4a6
Aimar <mendibil.ayo@gmail.com>
2026-05-30 20:07:27 +0200

    cambio en validacion de flags

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,22 +6,35 @@
 /*   By: amendibi <amendibi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 18:15:13 by amendibi          #+#    #+#             */
-/*   Updated: 2026/05/29 19:21:22 by amendibi         ###   ########.fr       */
+/*   Updated: 2026/05/30 19:24:55 by amendibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_validate_flag(char *flag)
+void	ft_validate_flag(t_stack *backpack, t_stats *totebag, int start)
 {
-	if (ft_strcmp(flag, "--simple"))
-		return (1);
-	if (ft_strcmp(flag, "--medium"))
-		return (2);
-	if (ft_strcmp(flag, "--complex"))
-		return (3);
-	if (ft_strcmp(flag, "--adaptative"))
-		return (4);
+	if (totebag->algorithm_flag == 0 || totebag->bench_flag == 0)
+	{
+		if (totebag->algorithm_flag == 0)
+		{
+			if (ft_strcmp(backpack->stack_a[start], "--simple"))
+				totebag->algorithm_flag = 1;
+			else if (ft_strcmp(backpack->stack_a[start], "--medium"))
+				totebag->algorithm_flag = 2;
+			else if (ft_strcmp(backpack->stack_a[start], "--complex"))
+				totebag->algorithm_flag = 3;
+			else if (ft_strcmp(backpack->stack_a[start], "--adaptative"))
+				totebag->algorithm_flag = 4;
+			else
+				ft_error_free(&backpack, &totebag);
+		}
+		if (totebag->bench_flag == 0)
+		{
+			if (ft_strcmp(backpack->stack_a[start], "--bench"))
+				totebag->bench_flag = 1;
+		}
+	}
 	ft_error();
 }
 
@@ -35,10 +48,10 @@ void	parse_args(int argc, char *argv[], t_stack *backpack, t_stats *totebag)
 		ft_error();
 	init_totebag(&totebag);
 	start = 1;
-	if (argv[1][0] == '-' && argv[1][1] == '-')
+	while (argv[start][0] == '-' && argv[start][1] == '-')
 	{
-		totebag->algorithm_flag = ft_validate_flag(argv[1]);
-		start = 2;
+		ft_validate_flag(&backpack, &totebag, start);
+		start++;
 	}
 	if (argc < start + 1)
 		ft_error();
@@ -70,8 +83,6 @@ int	main(int argc, char *argv[])
 			algorithm_medium(&backpack);
 		else if (totebag->algorithm_flag == 3)
 			algorithm_complex(&backpack);
-		else if (totebag->algorithm_flag == 4) // omitible
-			dispatch_adaptative(&backpack);
 	}
 	else
 		dispatch_adaptative(&backpack);
